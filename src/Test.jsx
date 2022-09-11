d6d427c0b48ceae3ab8a73509d9b253f240a2516
DzungDang <ddbdzung.wa@gmail.com>
2022-09-11 11:16:34 +0700

    init commit

---
+++ src/Test.jsx
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <div className="text-black bg-white text-center text-3xl">
+      Hello world from Tailwindcss
+    </div>
+  );
+}
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin']
+      }
+    })
+  ]
+});
